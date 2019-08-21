8e5500806ea2af78a1f65a21eaad385292cc3f98
BrijendraK <BrijendraK@LP-5CD8471476.HCLT.CORP.HCL.IN>
2019-08-21 13:34:42 +0530

    Changes in main application

--- src/main/java/com/ulta/product/ProductApplication.java
+++ src/main/java/com/ulta/product/ProductApplication.java
@@ -6,6 +6,8 @@
  */
 package com.ulta.product;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -19,9 +21,11 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:HystrixCommand.properties")
 public class ProductApplication {
-
+	static Logger log = LoggerFactory.getLogger(ProductApplication.class);
 	public static void main(String[] args) {
+		log.info("ProductApplication start");
 		SpringApplication.run(ProductApplication.class, args);
+		log.info("ProductApplication end");
 	}
 
 }
