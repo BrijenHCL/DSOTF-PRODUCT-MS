3c47628588056fc95dbd54f5598de994c4599f03
BrijendraK <BrijendraK@LP-5CD8471476.HCLT.CORP.HCL.IN>
2019-08-23 15:47:28 +0530

    Changes in swagger config.

--- src/main/java/com/ulta/product/config/SwaggerConfig.java
+++ src/main/java/com/ulta/product/config/SwaggerConfig.java
@@ -9,7 +9,8 @@ package com.ulta.product.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,17 +18,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
-	@SuppressWarnings("deprecation")
-	public static final ApiInfo DEFAULT_CUSTOM_INFO = new ApiInfo("Commerce Tool Product Microservice API Documents",
-			"Product Microservice API Documents", "1.0.0", "urn:tos", "brijendrak@hcl.com", "Cloud Native",
-			"http://www.pivotal.org/licenses/LICENSE-2.0");
-
 	@Bean
-	public Docket postsApi() {
-
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_CUSTOM_INFO);
-
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
 	}
 
 }
